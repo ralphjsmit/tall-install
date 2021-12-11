47249191d4554871357b5676fc29b8cf55724abe
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-11 17:13:01 +0100

    PHP CS Fixer test

--- src/TallInstallServiceProvider.php
+++ src/TallInstallServiceProvider.php
@@ -2,14 +2,15 @@
 
 namespace RalphJSmit\TallInstall;
 
-use RalphJSmit\TallInstall\Commands\TallInstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
+use RalphJSmit\TallInstall\Commands\TallInstallCommand;
 class TallInstallServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
+        if (true) {echo '';}
         $package
             ->name('tall-install')
             ->hasCommand(TallInstallCommand::class);

acdb4590ab8fbe9c433bbe6b065e1dec4065030a
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-11 17:00:33 +0100

    Testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,10 +11,6 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'RalphJSmit\\TallInstall\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
     }
 
     protected function getPackageProviders($app)
@@ -22,15 +18,5 @@ class TestCase extends Orchestra
         return [
             TallInstallServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_tall-install_table.php.stub';
-        $migration->up();
-        */
     }
 }

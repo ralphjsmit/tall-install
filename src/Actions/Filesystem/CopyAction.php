2fbf14442f064ad9942d97942dce1ee80c8a5e8c
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-24 15:56:36 +0100

    Update CopyAction with a MakeDirectoryAction

--- src/Actions/Filesystem/CopyAction.php
+++ src/Actions/Filesystem/CopyAction.php
@@ -2,14 +2,25 @@
 
 namespace RalphJSmit\TallInstall\Actions\Filesystem;
 
+use Illuminate\Support\Str;
 use RalphJSmit\TallInstall\Exceptions\Filesystem\CopyActionFailedException;
 
 class CopyAction
 {
+    public function __construct(
+        private MakeDirectoryAction $makeDirectoryAction,
+    ) {}
+
     public function execute(string $from, string $to, bool $overwrite = true): void
     {
         if ( $overwrite && file_exists($to) ) {
             unlink($to);
+        }
+
+        $directoryForNewFile = Str::beforeLast($to, '/');
+
+        if ( ! file_exists($directoryForNewFile) ) {
+            $this->makeDirectoryAction->execute($directoryForNewFile);
         }
 
         $result = copy($from, $to);

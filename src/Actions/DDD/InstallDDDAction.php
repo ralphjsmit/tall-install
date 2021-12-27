03dc715c3df691640f04e455b0ef02aa3f82625c
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-27 13:54:09 +0100

    Update namespaces for DDD

--- src/Actions/DDD/InstallDDDAction.php
+++ src/Actions/DDD/InstallDDDAction.php
@@ -2,27 +2,14 @@
 
 namespace RalphJSmit\TallInstall\Actions\DDD;
 
-use RalphJSmit\TallInstall\Actions\Composer\ComposerAction;
-use RalphJSmit\TallInstall\Actions\Filesystem\FileGetContentsAction;
-use RalphJSmit\TallInstall\Actions\Filesystem\FilePutContentsAction;
-
 class InstallDDDAction
 {
     public function __construct(
-        private ComposerAction $composerAction,
-        private FileGetContentsAction $fileGetContentsAction,
-        private FilePutContentsAction $filePutContentsAction,
+        private UpdateNamespacesAction $updateNamespacesAction,
     ) {}
 
     public function execute(string $basePath)
     {
-        $composer = json_decode($this->fileGetContentsAction->execute($basePath . '/composer.json'), true);
-
-        $composer['autoload']['psr-4']['App\\'] = 'src/App/';
-        $composer['autoload']['psr-4']['Domain\\'] = 'src/Domain/';
-        $composer['autoload']['psr-4']['Support\\'] = 'src/Support/';
-        $composer['autoload']['files'][0] = 'src/Support/helpers.php';
-
-        $this->filePutContentsAction->execute($basePath . '/composer.json', json_encode((array) $composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        $this->updateNamespacesAction->execute($basePath);
     }
 }
---
+++ src/Actions/DDD/UpdateNamespacesAction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace RalphJSmit\TallInstall\Actions\DDD;
+
+use Illuminate\Support\Arr;
+use RalphJSmit\TallInstall\Actions\Filesystem\FileGetContentsAction;
+use RalphJSmit\TallInstall\Actions\Filesystem\FilePutContentsAction;
+
+class UpdateNamespacesAction
+{
+    public function __construct(
+        private FileGetContentsAction $fileGetContentsAction,
+        private FilePutContentsAction $filePutContentsAction,
+    ) {}
+
+    public function execute(string $basePath)
+    {
+        $composer = json_decode($this->fileGetContentsAction->execute($basePath . '/composer.json'), true);
+
+        $composer['autoload']['psr-4'] = collect([
+            'App\\' => 'src/App/',
+            'Domain\\' => 'src/Domain/',
+            'Support\\' => 'src/Support/',
+        ])->merge(
+            Arr::except($composer['autoload']['psr-4'], 'App\\')
+        )->toArray();
+
+        $composer['autoload']['files'] = [
+            'src/Support/helpers.php',
+        ];
+
+        $this->filePutContentsAction->execute(
+            $basePath . '/composer.json',
+            json_encode((array) $composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+        );
+    }
+}

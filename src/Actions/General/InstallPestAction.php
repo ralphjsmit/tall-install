a6ae10c7b29289b1552521c2f2ad6a2d376c2dd3
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-28 13:33:42 +0100

    Run pest:install without interaction

--- src/Actions/General/InstallPestAction.php
+++ src/Actions/General/InstallPestAction.php
@@ -10,17 +10,16 @@ class InstallPestAction
 {
     public function __construct(
         private ComposerInstallAction $composerInstallAction
-    ) {
-    }
+    ) {}
 
     public function execute(string $basePath): void
     {
         $this->composerInstallAction->execute(['pestphp/pest-plugin-laravel', '--dev'], $basePath);
 
-        $process = new Process(['php', 'artisan', 'pest:install'], $basePath);
+        $process = new Process(['php', 'artisan', 'pest:install', '--no-interaction'], $basePath);
         $process->run();
 
-        if (! $process->isSuccessful()) {
+        if ( ! $process->isSuccessful() ) {
             throw new PestInstallFailedException($process);
         }
     }

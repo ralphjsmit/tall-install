011e00d288e07588a31a95cc3c6cec7449809175
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-28 13:33:17 +0100

    Refactor to Filesystem actions

--- src/Actions/General/SetupBrowsersyncAction.php
+++ src/Actions/General/SetupBrowsersyncAction.php
@@ -3,21 +3,26 @@
 namespace RalphJSmit\TallInstall\Actions\General;
 
 use Illuminate\Support\Str;
-
-use function RalphJSmit\PestPluginFilesystem\contents;
+use RalphJSmit\TallInstall\Actions\Filesystem\FileGetContentsAction;
+use RalphJSmit\TallInstall\Actions\Filesystem\FilePutContentsAction;
 
 class SetupBrowsersyncAction
 {
+    public function __construct(
+        private FileGetContentsAction $fileGetContentsAction,
+        private FilePutContentsAction $filePutContentsAction,
+    ) {}
+
     public function execute(string $basePath, string $domain = null): void
     {
-        $webpackMixJsContents = Str::of(contents($basePath . '/webpack.mix.js'));
+        $webpackMixJsContents = Str::of($this->fileGetContentsAction->execute($basePath . '/webpack.mix.js'));
 
         $domain = $domain ? Str::of($domain)->remove('.test') : Str::of($basePath)->rtrim('/')->afterLast('/');
 
         $webpackMixJsContents = $webpackMixJsContents->append(
-            Str::replace('valetDomain', $domain, contents(stub('webpack.mix.js.browsersync')))
+            Str::replace('valetDomain', $domain, $this->fileGetContentsAction->execute(stub('webpack.mix.js.browsersync')))
         );
 
-        file_put_contents($basePath . '/webpack.mix.js', $webpackMixJsContents);
+        $this->filePutContentsAction->execute($basePath . '/webpack.mix.js', $webpackMixJsContents);
     }
 }

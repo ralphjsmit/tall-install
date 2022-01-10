737afced61f664141c8dfa87d4248d0bef1394ef
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-01-10 11:41:33 +0100

    Delete unnecessary dump-statement

--- src/Actions/Filesystem/FilePutContentsAction.php
+++ src/Actions/Filesystem/FilePutContentsAction.php
@@ -6,8 +6,6 @@ class FilePutContentsAction
 {
     public function execute(string $path, string $contents): string
     {
-        dump('Contents', $contents);
-
         return file_put_contents($path, $contents);
     }
 }
